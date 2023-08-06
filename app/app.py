fa7174120ef90e840b496352018f8e136e5817f4
tomokiito <tomoki.ito.1004@gmail.com>
2023-08-07 08:44:42 +0900

    modified:   app/app.py

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
-from app.forms import SearchForm
-from app.services import get_stations_near
+from .forms import SearchForm
+from .services import get_stations_near
 
 import os
 from dotenv import load_dotenv
