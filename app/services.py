8a96f544c949553348a96cb7f5c16d7ba0533625
tomokiito <tomoki.ito.1004@gmail.com>
2023-08-07 11:29:00 +0900

    modified:   app/services.py

--- app/services.py
+++ app/services.py
@@ -2,14 +2,51 @@ import os
 from dotenv import load_dotenv
 import requests
 
+
+def get_value_safe(data, *keys):
+    try:
+        for key in keys:
+            if isinstance(data, list) and isinstance(key, int) and key < len(data):
+                data = data[key]
+            elif isinstance(data, dict) and key in data:
+                data = data[key]
+            else:
+                return None
+        return data
+    except TypeError:
+        return None
+
 def get_charging_stations(api_key, lat, lon):
     distance_unit = "KM"
     distance = 10
     url = f"https://api.openchargemap.io/v3/poi/?key={api_key}&latitude={lat}&longitude={lon}&distanceunit={distance_unit}&distance={distance}"
     response = requests.get(url)
     data = response.json()
-    stations = [item['AddressInfo']['Title'] for item in data]
+
+    stations = []
+    for item in data:
+        station_info = {
+            "Title": get_value_safe(item, 'AddressInfo', 'Title'),
+            "ID": get_value_safe(item, 'ID'),
+            "ConnectionTypeID": get_value_safe(item, 'Connections', 0, 'ConnectionTypeID'),
+            "ConnectionType": get_value_safe(item, 'Connections', 0, 'ConnectionType', 'Title'),
+            "Reference": get_value_safe(item, 'Connections', 0, 'Reference'),
+            "StatusTypeID": get_value_safe(item, 'StatusType', 'ID'),
+            "StatusType": get_value_safe(item, 'StatusType', 'Title'),
+            "LevelID": get_value_safe(item, 'Connections', 0, 'LevelID'),
+            "Level": get_value_safe(item, 'Connections', 0, 'Level', 'Title'),
+            "Amps": get_value_safe(item, 'Connections', 0, 'Amps'),
+            "Voltage": get_value_safe(item, 'Connections', 0, 'Voltage'),
+            "PowerKW": get_value_safe(item, 'Connections', 0, 'PowerKW'),
+            "CurrentTypeID": get_value_safe(item, 'Connections', 0, 'CurrentTypeID'),
+            "CurrentType": get_value_safe(item, 'Connections', 0, 'CurrentType', 'Title'),
+            "Quantity": get_value_safe(item, 'Connections', 0, 'Quantity'),
+            "Comments": get_value_safe(item, 'Connections', 0, 'Comments')
+        }
+        stations.append(station_info)
     return stations
+
+
 
 def get_geocode(address, api_key):
     url = f"https://us1.locationiq.com/v1/search.php?key={api_key}&q={address}&format=json"
